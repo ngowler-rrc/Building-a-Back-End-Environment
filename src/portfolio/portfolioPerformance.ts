ca233b52912ca43bbbb9ba18b0dd5c3b5a11746e
Nick Gowler <ngowler@academic.rrc.ca>
2025-01-19 18:38:42 -0600

    Create function to find largest holding and asset allocation percentages.

--- src/portfolio/portfolioPerformance.ts
+++ src/portfolio/portfolioPerformance.ts
@@ -3,7 +3,7 @@ export interface PortfolioPerformance {
     currentValue: number;
     profitOrLoss: number;
     percentageChange: number;
-    performanceSummary: string
+    performanceSummary: string;
 }
 
 export function calculatePortfolioPerformance(initialInvestment: number, currentValue: number): PortfolioPerformance {
@@ -32,14 +32,14 @@ export function calculatePortfolioPerformance(initialInvestment: number, current
         lossSignificant: `The portfolio has lost significantly with a loss of $${profitOrLoss}.`
     };
 
-    let performanceSummary =
+    let performanceSummary: string =
     percentageChange > 20 ? summaries.gainSignificant :
     percentageChange > 10 ? summaries.gainModerate :
     percentageChange > 0.1 ? summaries.gainSlightly :
     percentageChange === 0 ? summaries.noChange :
-    percentageChange < -0.1 ? summaries.lossSlightly :
-    percentageChange < -10 ? summaries.lossModerate :
     percentageChange < -20 ? summaries.lossSignificant :
+    percentageChange < -10 ? summaries.lossModerate :
+    percentageChange < -0.1 ? summaries.lossSlightly :
     summaries.noChange;
 
 	const portfolioPerformance: PortfolioPerformance = {
@@ -47,8 +47,45 @@ export function calculatePortfolioPerformance(initialInvestment: number, current
 		currentValue,
 		profitOrLoss,
 		percentageChange,
-		performanceSummary,
+		performanceSummary
 	}
 
     return portfolioPerformance;
+}
+
+export interface Asset {
+    name: string;
+    value: number;
+}
+
+export function findLargestHolding(assets: Asset[]): Asset {
+    let largestHolding: Asset = assets[0];
+
+    for (let i = 1; i < assets.length; i++) {
+        if (assets[i].value > largestHolding.value) {
+            largestHolding = assets[i];
+        }
+    }
+    
+    return largestHolding;
+}
+
+export interface AssetAllocation {
+    name: string;
+    percentage: number;
+}
+
+export function findAssetAllocationPercentage(assets: Asset[]): AssetAllocation[] {
+    let assetAllocationPercentages: AssetAllocation[] = [];
+    let totalValue: number = 0;
+
+    assets.forEach(asset => {
+        totalValue += asset.value;
+    });
+    assets.forEach(asset => {
+        const percentage: number = (asset.value / totalValue) * 100;
+        assetAllocationPercentages.push({ name: asset.name, percentage });
+    });
+
+    return assetAllocationPercentages;
 }
